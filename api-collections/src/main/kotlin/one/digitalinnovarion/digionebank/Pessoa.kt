8a63c07129fed3df0a4d03b7c9f97ac13271a058
Julio Henrique de Oliveira <julioholiveira7@gmail.com>
2022-06-27 21:05:15 -0300

    Nova versão do código Bank

--- api-collections/src/main/kotlin/one/digitalinnovarion/digionebank/Pessoa.kt
+++ api-collections/src/main/kotlin/one/digitalinnovarion/digionebank/Pessoa.kt
@@ -1,12 +1,27 @@
 package one.digitalinnovarion.digionebank
 
+//Classe Pessoa que irá conter os dados de uma pessoa.
 class Pessoa {
     var nome: String = "Julio Henrique de Oliveira"
-    var cpf: String = "095.458.469-41"
+    var cpf: String = "123.456.789-10" //Dessa forma, não é possível no Main selecionar esse variável
+    private set
+
+    //Esse declaração de uma classe dentro de outra classe não é recomendado.
+    inner class Endereco {
+        var rua: String = "Rua Teste, 123"
+    }
 }
 
+//Classe principal
 fun main() {
     val julio = Pessoa()
-    println("O nome da criatura é ${julio.nome}")
-    println("O CPF da criatura é ${julio.cpf}")
+
+    //julio.cpf = "123" Como o set está private, não conseguimos setar nada nas variáveis
+
+    println("O nome da criatura é: ${julio.nome}")
+    println("O CPF da criatura é: ${julio.cpf}")
+
+    //Como puxar da classe inner, dentro da classe Pessoas.
+    println("O endereço da criatura é: ${julio.Endereco().rua}")
+
 }
